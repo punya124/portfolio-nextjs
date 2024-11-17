09abc4d03fb8a0daa6240e0a9b18e2fe458bec14
Punya Galundia <spypunya@gmail.com>
2024-11-17 14:47:46 -0500

    clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav><h1>Portfolio Website</h1></nav>
         {children}
       </body>
     </html>
