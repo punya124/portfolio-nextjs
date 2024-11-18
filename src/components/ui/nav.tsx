35938a8c3155c9994b526546de298fd21ab58a67
Punya Galundia <spypunya@gmail.com>
2024-11-18 17:40:58 -0500

    nav-add

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -1,17 +1,37 @@
 "use client"
-
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { ModeToggle } from "../mode-toggle"
+import { Underline } from 'lucide-react'
 
-export default function Nav() {
+const links = [
+    {
+        name: "home",
+        path: "/"
+    },
+    {
+        name: "resume",
+        path: "/#resume"
+    },
+    {
+        name: "work",
+        path: "/#work"
+    },
+    {
+        name: "contact",
+        path: "/#contact"
+    },
+]
+
+const Nav = () => {
+    const pathname = usePathname();
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <ModeToggle />
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <nav className='flex gap-8'>
+            {links.map((link, index) => {
+                return <Link href={link.path} key={index}>{link.name}</Link>
+            })}
+        </nav>
     )
 }
+
+export default Nav
