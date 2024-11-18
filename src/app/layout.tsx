12ba14d08633b917d9b5bf1e38c98dc04beddb5a
Punya Galundia <spypunya@gmail.com>
2024-11-18 17:41:19 -0500

    update-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import Header from "@/components/ui/header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const fontName = Quicksand({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,13 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={fontName.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+
+          <Header />
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Nav from "@/components/ui/nav";
 
 export default function Home() {
   return (
-    <main className="flex p-16">
-      <Nav />
-      <section className="py-24 flex flex-col gap-8">
-        <h1>Punya G</h1>
+    <main className="p-16">
+      <section className="py-24 flex flex-col gapx-8">
+        <h1 className="text-4xl font-bold">John Doe</h1>
+        <p className="text-base text-muted-foreground">Web Developer</p>
+        <p className="text-base text-muted-foreground">Amherst, MA</p>
       </section>
     </main>
   );
