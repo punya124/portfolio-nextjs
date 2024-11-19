4768a80f25cd4e6298716c58fe7dcc7cc97dfd9c
Punya Galundia <spypunya@gmail.com>
2024-11-19 13:48:12 -0500

    heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import Nav from "@/components/ui/nav";
 export default function Home() {
   return (
     <main className="p-16">
-      <section className="py-24 flex flex-col gapx-8">
-        <h1 className="text-4xl font-bold">John Doe</h1>
-        <p className="text-base text-muted-foreground">Web Developer</p>
-        <p className="text-base text-muted-foreground">Amherst, MA</p>
+      <section className="flex flex-row align-middle items-center justify-between">
+        <div className="py-24 flex flex-col gapx-8">
+          <h1 className="text-4xl font-bold">John Doe</h1>
+          <p className="text-base text-muted-foreground">Web Developer</p>
+          <p className="text-base text-muted-foreground">Amherst, MA</p>
+        </div>
+        <div>
+          <div className="bg-white w-48 h-48 rounded-3xl" />
+        </div>
       </section>
     </main>
   );
